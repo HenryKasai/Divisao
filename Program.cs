a0bb33bd48d113edf1de82b453eda0a7d2b3c68f
HenryKasai <henrykasai1775@gmail.com>
2020-06-05 20:13:45 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Insira o numerador da divisão: ");
+            decimal numerador = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Insira o denominador da divisão: ");
+            decimal denominador = Convert.ToDecimal(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (denominador == 0)
+            {
+                Console.WriteLine("Não é possível dividir por zero.");
+            }
+            else 
+            {
+                decimal resultado = numerador / denominador;
+                Console.WriteLine($"{numerador} dividido por {denominador} é igual a {resultado}.");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
